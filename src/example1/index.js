51283d0a42a76d70eecf157a84e660abbc0864b2
YI <differentialmanifold@gmail.com>
2018-04-16 17:53:32 +0800

    first commit

---
+++ src/example1/index.js
@@ -0,0 +1,115 @@
+import Vue from 'vue'
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+})
+
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+
+var app4 = new Vue({
+    el: '#app-4',
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Learn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    }
+})
+
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
+
+
+Vue.component('todo-item', {
+    // The todo-item component now accepts a
+    // "prop", which is like a custom attribute.
+    // This prop is called todo.
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+})
+
+var getChildrenTextContent = function (children) {
+    return children.map(function (node) {
+        return node.children
+            ? getChildrenTextContent(node.children)
+            : node.text
+    }).join('')
+}
+
+Vue.component('anchored-heading', {
+    render: function (createElement) {
+        // create kebabCase id
+        var headingId = getChildrenTextContent(this.$slots.default)
+            .toLowerCase()
+            .replace(/\W+/g, '-')
+            .replace(/(^\-|\-$)/g, '')
+
+        return createElement(
+            'h' + this.level,
+            [
+                createElement('a', {
+                    attrs: {
+                        name: headingId,
+                        href: '#' + headingId
+                    }
+                }, this.$slots.default)
+            ]
+        )
+    },
+    props: {
+        level: {
+            type: Number,
+            required: true
+        }
+    }
+})
+
+Vue.component('blog-post', {
+    render: function (createElement) {
+        var header = this.$slots.header
+        var body = this.$slots.default
+        var footer = this.$slots.footer
+        return createElement('div', [
+            createElement('header', header),
+            createElement('main', body),
+            createElement('footer', footer)
+        ])
+    }
+})
+
+var app6 = new Vue({
+    el: '#app-6',
+    data: {
+        message: 'Hello Vue!',
+        groceryList: [
+            { id: 0, text: 'Vegetables' },
+            { id: 1, text: 'Cheese' },
+            { id: 2, text: 'Whatever else humans are supposed to eat' }
+        ]
+    }
+})
